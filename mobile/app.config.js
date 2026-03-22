2741629eff7aa88fd5f6141c9ed64d18e5e4f43f
Vidac <vidac@appel.com>
2026-03-22 11:55:00 +0100

    fix: use same slug for staging and production EAS builds

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -5,7 +5,7 @@ module.exports = ({ config }) => ({
   ...config,
   expo: {
     name: isStaging ? "APPEL TEST" : "APPEL",
-    slug: isStaging ? "mobile-staging" : "mobile",
+    slug: "mobile",
     version: "2.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
